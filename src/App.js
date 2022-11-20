e6e46a717c2a379a32c618f4826aee01c535b10e
Roshan Tadadikar <71004273+Roshan-Tadadikar@users.noreply.github.com>
2022-11-20 18:53:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,175 @@
+import { useState } from "react";
+import "./styles.css";
+
+var EmjoiDict = {
+  "🍇": "Grapes",
+  "🍈": "Melon",
+  "🍉": "Watermelon",
+  "🍊": "Tangerine",
+  "🍋": " Lemon",
+  "🍌": "Banana",
+  "🍍": "Pineapple",
+  "🥭": "Mango",
+  "🍎": "Red Apple",
+  "🍏": "Green Apple",
+  "🍐": "Pear",
+  "🍑": "Peach",
+  "🍒": "Cherries",
+  "🍓": "Strawberry",
+  "🫐": "Blueberries",
+  "🥝": "Kiwi Fruit",
+  "🍅": "Tomato",
+  "🫒": "Olive",
+  "🥥": "Coconut",
+  "🥑": "Avocado",
+  "🍆": "Eggplant",
+  "🥔": "Potato",
+  "🥕": "Carrot",
+  "🌽": "Ear of Corn",
+  "🫑": "Bell Pepper",
+  "🥒": "Cucumber",
+  "🥬": "Leafy Green",
+  "🥦": "Broccoli",
+  "🧄": "Garlic",
+  "🧅": "Onion",
+  "🍄": "Mushroom",
+  "🥜": "Peanuts",
+  "🫘": "Beans",
+  "🌰": "Chestnut",
+  "🍞": "Bread",
+  "🥐": "Croissant",
+  "🥖": "Baguette Bread",
+  "🫓": "Flatbread",
+  "🥨": "Pretzel",
+  "🥯": "Bagel",
+  "🥞": "Pancakes",
+  "🧇": "Waffle",
+  "🧀": "Cheese Wedge",
+  "🍖": "Meat on Bone",
+  "🍗": "Poultry Leg",
+  "🥩": "Cut of Meat",
+  "🥓": "Bacon",
+  "🍔": "Hamburger",
+  "🍟": "French Fries",
+  "🍕": " Pizza",
+  "🌭": "Hot Dog",
+  "🥪": "Sandwich",
+  "🌮": "Taco",
+  "🌯": "Burrito",
+  "🫔": "Tamale",
+  "🥙": "Stuffed Flatbread",
+  "🧆": "Falafel",
+  "🥚": "Egg",
+  "🍳": "Cooking",
+  "🥘": "Shallow Pan of Food",
+  "🍲": "Pot of Food",
+  "🫕": "Fondue",
+  "🥣": "Bowl with Spoon",
+  "🥗": "Green Salad",
+  "🍿": "Popcorn",
+  "🧈": "Butter",
+  "🧂": "Salt",
+  "🥫": "Canned Food",
+  "🍱": "Bento Box",
+  "🍘": "Rice Cracker",
+  "🍙": "Rice Ball",
+  "🍚": "Cooked Rice",
+  "🍛": "Curry Rice",
+  "🍜": "Steaming Bowl",
+  "🍝": "Spaghetti",
+  "🍠": "Roasted Sweet Potato",
+  "🍢": "Oden",
+  "🍣": "Sushi",
+  "🍤": "Fried Shrimp",
+  "🍥": "Fish Cake with Swirl",
+  "🥮": "Moon Cake",
+  "🍡": "Dango",
+  "🥟": "Dumpling",
+  "🥠": "Fortune Cookie",
+  "🥡": " Takeout Box",
+  "🦪": "Oyster",
+  "🍦": "Soft Ice Cream",
+  "🍧": "Shaved Ice",
+  "🍨": "Ice Cream",
+  "🍩": "Doughnut",
+  "🍪": "Cookie",
+  "🎂": "Birthday Cake",
+  "🍰": "Shortcake",
+  "🧁": "Cupcake",
+  "🥧": "Pie",
+  "🍫": "Chocolate Bar",
+  "🍬": "Candy",
+  "🍭": "Lollipop",
+  "🍮": "Custard",
+  "🍯": "Honey Pot",
+  "🍼": "Baby Bottle",
+  "🥛": "Glass of Milk",
+  "☕": "Hot Beverage",
+  "🫖": "Teapot",
+  "🍵": "Teacup Without Handle",
+  "🍶": " Sake",
+  "🍾": "Bottle with Popping Cork",
+  "🍷": "Wine Glass",
+  "🍸": "Cocktail Glass",
+  "🍹": "Tropical Drink",
+  "🍺": "Beer Mug",
+  "🍻": "Clinking Beer Mugs",
+  "🥂": "Clinking Glasses",
+  "🥃": "Tumbler Glass",
+  "🫗": "Pouring Liquid",
+  "🥤": "Cup with Straw",
+  "🧋": "Bubble Tea",
+  "🧃": "Beverage Box",
+  "🧉": "Mate",
+  "🧊": "Ice",
+  "🥢": "Chopsticks",
+  "🍴": "Fork and Knife",
+  "🥄": "Spoon",
+  "🫙": "Jar"
+};
+
+var emojisweKow = Object.keys(EmjoiDict);
+
+export default function App() {
+  var [suerMeaning, setMeaning] = useState("");
+  function likeclickhandler(event) {
+    var userInput = event.target.value;
+
+    var meaning = EmjoiDict[userInput];
+    if (meaning === undefined) {
+      meaning = "we dont have this in our database";
+    }
+    setMeaning(meaning);
+  }
+
+  function emojiclickHandler(emoji) {
+    var emomeaning = EmjoiDict[emoji];
+    setMeaning(emomeaning);
+  }
+
+  return (
+    <div className="App">
+      <h1> Food and Cutleries</h1>
+      <input
+        style={{ borderRadius: "20px" }}
+        onChange={likeclickhandler}
+        placeholder="put an emoji to know the meaningn of it"
+      />
+      <h2>{suerMeaning}</h2>
+      <h3>Emojis we know</h3>
+      <div>
+        {emojisweKow.map(function (emoji) {
+          return (
+            <span
+              onClick={() => emojiclickHandler(emoji)}
+              style={{ fontSize: "2rem", padding: "0.5 rem" }}
+              key={emoji}
+            >
+              {emoji}
+            </span>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
